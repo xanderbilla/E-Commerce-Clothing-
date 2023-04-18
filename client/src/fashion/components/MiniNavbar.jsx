8f8f33c8561dd965ad984212a6325d6f3a915089
VIKAS SINGH <xanderbilla@gmail.com>
2023-04-18 13:57:53 +0530

    Added MiniNavbar

---
+++ client/src/fashion/components/MiniNavbar.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import styles from '../styles/miniNavbar.module.css'
+import LaunchIcon from '@mui/icons-material/Launch';
+
+const MiniNavbar = () => {
+    return (
+        <div className={styles.mininavbar}>
+            <div className={styles.left}>
+                <select className={styles.all_categories} name="categories" title="categories">
+                    <option className={styles.categories__selectOption} selected disabled value="All">All Categories</option>
+                    <option className={styles.categories__selectOption} value="Men">Men</option>
+                    <option className={styles.categories__selectOption} value="Women">Women</option>
+                    <option className={styles.categories__selectOption} value="Kids">Kids</option>
+                    <option className={styles.categories__selectOption} value="Accessories">Accessories</option>
+                </select>
+            </div>
+            <div className={styles.right}>
+                <div className={styles.links}>
+                 Food <LaunchIcon fontSize='small'/>
+                </div>
+                <div className={styles.links}>
+                    Grocery <LaunchIcon fontSize='small'/>
+                </div>
+                <div className={styles.links}>
+                    Electronics <LaunchIcon fontSize='small'/>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default MiniNavbar
