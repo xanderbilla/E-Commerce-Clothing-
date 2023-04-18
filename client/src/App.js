c6a2f49b0cbe14c2a6d5e60a5989c2e5bbf5e23e
VIKAS SINGH <xanderbilla@gmail.com>
2023-04-18 13:54:33 +0530

    App.js

---
+++ client/src/App.js
@@ -0,0 +1,36 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { useState } from 'react'
+import { Cart, LoginPage, Main, SignUpPage, ProductList, ProductPage, Verify } from './fashion/Routes'
+import Navbar from './fashion/components/Navbar'
+import Footer from './fashion/components/Footer'
+import MiniNavbar from './fashion/components/MiniNavbar';
+
+function App() {
+  const [isOpen, setIsOpen] = useState(false);
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Navbar isOpen={isOpen} setIsOpen={setIsOpen} />
+        <MiniNavbar/>
+        <Routes>
+          <Route path="/">
+            <Route path='fashion'>
+              <Route index element={<Main />} />
+              <Route path='cart' element={<Cart />} />
+              <Route path='signup' element={<SignUpPage />} />
+              <Route path='signin' element={<LoginPage />} />
+              <Route path='verify' element={<Verify />} />
+              <Route path='product/:productId' element={<ProductPage />} />
+              <Route path=':catName' >
+                <Route index element={<ProductList />} />
+              </Route>
+            </Route>
+          </Route>
+        </Routes>
+      </BrowserRouter>
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
