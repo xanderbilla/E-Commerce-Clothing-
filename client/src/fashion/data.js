15e0a0d3fb4668e955f718d6537ec2f3bd286ade
VIKAS SINGH <xanderbilla@gmail.com>
2023-04-18 13:54:45 +0530

    Static Data

---
+++ client/src/fashion/data.js
@@ -0,0 +1,168 @@
+export const sliderItems = [
+  {
+    id: 1,
+    img: "https://i.imgur.com/Jr1lNp9.png",
+    title: "SUMMER SALE",
+    desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+    bg: "f5fafd",
+  },
+  {
+    id: 2,
+    img: "https://i.imgur.com/yRBHphH.png",
+    title: "AUTUMN COLLECTION",
+    desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+    bg: "fcf1ed",
+  },
+  {
+    id: 3,
+    img: "https://i.imgur.com/G4t3S3c.png",
+    title: "LOUNGEWEAR LOVE",
+    desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+    bg: "fbf0f4",
+  },
+];
+
+export const categories = [
+  {
+    id: 1,
+    img: "https://images.pexels.com/photos/5886041/pexels-photo-5886041.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+    title: "SHIRT STYLE!",
+    cat: "Women"
+  },
+  {
+    id: 2,
+    img: "https://images.pexels.com/photos/2983464/pexels-photo-2983464.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+    title: "LOUNGEWEAR LOVE",
+    cat: "Men"
+  },
+  {
+    id: 3,
+    img: "https://images.pexels.com/photos/5480696/pexels-photo-5480696.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+    title: "LIGHT JACKETS",
+    cat: "Accessories"
+  },
+];
+
+export const featuredProducts = [
+  {
+    _id: 1,
+    img: "https://images.pexels.com/photos/1972115/pexels-photo-1972115.jpeg?auto=compress&cs=tinysrgb&w=1600",
+    img2: "https://images.pexels.com/photos/1163194/pexels-photo-1163194.jpeg?auto=compress&cs=tinysrgb&w=1600",
+    title: "Long Sleeve Graphic T-Shirt",
+    isNew: true,
+    oldPrice: 19,
+    category: "Women",
+    price: 12,
+    size: ["M", "S", "XS"],
+    color: ["Yellow", "Black", "Green"]
+  },
+  {
+    _id: 2,
+    img: "https://images.pexels.com/photos/1759622/pexels-photo-1759622.jpeg?auto=compress&cs=tinysrgb&w=1600",
+    title: "Coat",
+    isNew: true,
+    oldPrice: 19,
+    category: "Women",
+    price: 12,
+    size: ["M", "L", "XL", "XS"],
+    color: ["Yellow", "Blue", "Red"]
+
+  },
+  {
+    _id: 3,
+    img: "https://images.pexels.com/photos/1457983/pexels-photo-1457983.jpeg?auto=compress&cs=tinysrgb&w=1600",
+    title: "Skirt",
+    oldPrice: 19,
+    category: "Women",
+    price: 12,
+    size: ["M", "L", "M"],
+    color: ["Red", "White", "Green"]
+  },
+  {
+    _id: 4,
+    img: "https://images.pexels.com/photos/2065200/pexels-photo-2065200.jpeg?auto=compress&cs=tinysrgb&w=1600",
+    title: "Trouser",
+    oldPrice: 19,
+    category: "Men",
+    price: 12,
+    size: ["M", "XL", "XS"],
+    color: ["Blue", "Black", "White"]
+  }
+]
+
+export const popularProducts = [
+  {
+    _id: 1,
+    img: "https://images.pexels.com/photos/1972115/pexels-photo-1972115.jpeg?auto=compress&cs=tinysrgb&w=1600",
+    img2: "https://images.pexels.com/photos/1163194/pexels-photo-1163194.jpeg?auto=compress&cs=tinysrgb&w=1600",
+    title: "Long Sleeve Graphic T-Shirt",
+    isNew: true,
+    oldPrice: 19,
+    category: "Women",
+    price: 12,
+    size: ["M", "S", "XS"],
+    color: ["Yellow", "Black", "Green"]
+  },
+  {
+    _id: 2,
+    img: "https://images.pexels.com/photos/1759622/pexels-photo-1759622.jpeg?auto=compress&cs=tinysrgb&w=1600",
+    title: "Coat",
+    isNew: true,
+    oldPrice: 19,
+    category: "Women",
+    price: 12,
+    size: ["M", "L", "XL", "XS"],
+    color: ["Yellow", "Blue", "Red"]
+
+  },
+  {
+    _id: 3,
+    img: "https://images.pexels.com/photos/1457983/pexels-photo-1457983.jpeg?auto=compress&cs=tinysrgb&w=1600",
+    title: "Skirt",
+    oldPrice: 19,
+    category: "Women",
+    price: 12,
+    size: ["M", "L", "M"],
+    color: ["Red", "White", "Green"]
+  },
+  {
+    _id: 4,
+    img: "https://images.pexels.com/photos/2065200/pexels-photo-2065200.jpeg?auto=compress&cs=tinysrgb&w=1600",
+    title: "Trouser",
+    oldPrice: 19,
+    category: "Men",
+    price: 12,
+    size: ["M", "XL", "XS"],
+    color: ["Blue", "Black", "White"]
+  },
+  {
+    _id: 5,
+    img: "https://images.pexels.com/photos/769749/pexels-photo-769749.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    title: "Shirt",
+    oldPrice: 19,
+    category: "Men",
+    price: 12,
+    size: ["M", "S", "XS"],
+    color: ["Blue", "Yellow", "White"]
+  },
+  {
+    _id: 6,
+    img: "https://images.pexels.com/photos/1346187/pexels-photo-1346187.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    title: "Jeans",
+    oldPrice: 19,
+    category: "Men",
+    price: 12,
+    size: ["M", "S", "L", "XL"],
+    color: ["Black", "White"]
+  },
+  {
+    _id: 7,
+    img: "https://images.pexels.com/photos/984619/pexels-photo-984619.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    title: "Hat",
+    oldPrice: 23,
+    category: "Accessories",
+    price: 20,
+    size: ["M", "S"],
+    color: ["Blue"]
+  },
+]
