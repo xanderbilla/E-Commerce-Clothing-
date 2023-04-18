f280e3d58b4f1756ae1f090d8489c483c340d0dd
VIKAS SINGH <xanderbilla@gmail.com>
2023-04-18 13:55:24 +0530

    Added Announcement Component

---
+++ client/src/fashion/components/Announcement.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from '../styles/announcement.module.css'
+
+const Announcement = () => {
+  return (
+    <div className={styles.container}>
+      Super Deal! Free Shopping on Order over $1200
+    </div>
+  )
+}
+
+export default Announcement
