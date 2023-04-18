f89dd641d0079904069fecf687cb060d93d3ab12
VIKAS SINGH <xanderbilla@gmail.com>
2023-04-18 13:54:53 +0530

    Routes.js

---
+++ client/src/fashion/Routes.js
@@ -0,0 +1,9 @@
+import Main from './pages/Main';
+import ProductList from './pages/ProductList';
+import ProductPage from './pages/ProductPage';
+import LoginPage from './pages/LoginPage';
+import SignUpPage from './pages/SignUpPage';
+import Cart from './pages/Cart';
+import Verify from './components/Verify';
+
+export {Cart, LoginPage, Main, SignUpPage, ProductList, ProductPage, Verify}
