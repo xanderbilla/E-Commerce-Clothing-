ca0314b643547aaafd437516782bcf2c9b06808f
VIKAS SINGH <xanderbilla@gmail.com>
2023-04-26 04:09:54 +0530

    App.js

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { useState } from 'react';
+import { Cart, LoginPage, Main, SignUpPage, ProductList, ProductPage, User, Verify, ProfilePage } from './static/Routes';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+import MiniNavbar from './components/MiniNavbar';
+
+function App() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Navbar isOpen={isOpen} setIsOpen={setIsOpen} />
+        <MiniNavbar />
+        <Routes>
+          <Route path="/">
+            <Route index element={<Main />} />
+            <Route path='cart' element={<Cart />} />
+            <Route path='my-profile' element={<ProfilePage />} />
+            <Route path='verify' element={<Verify />} />
+            <Route path='user' element={<User />} />
+            <Route path='product/:productId' element={<ProductPage />} />
+            <Route path='/category/:catName' >
+              <Route index element={<ProductList />} />
+            </Route>
+            <Route path='signup' element={<SignUpPage />} />
+            <Route path='signin' element={<LoginPage />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
