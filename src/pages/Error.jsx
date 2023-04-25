bfb3ad04c5e41ec6a5ecc0bf78292520c40a3303
VIKAS SINGH <xanderbilla@gmail.com>
2023-04-26 05:14:53 +0530

    C2# Major CSS Bug Fixed

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
-import '../styles/error.css'
+import styles from '../styles/error.module.css'
 const Error = () => {
   return (
-    <main>
-  <h1>4<span>0</span>4</h1>
-  <h2>Error: 404 page not found</h2>
-  <p>Sorry, the page you're looking for cannot be accessed</p>
+    <main className={styles.container}>
+  <h1 className={styles.heading}>4<span className={styles.span}>0</span>4</h1>
+  <h2 className={styles.sub_heading}>Error: 404 page not found</h2>
+  <p className={styles.para}>Sorry, the page you're looking for cannot be accessed</p>
 </main>
   )
 }
