2d6896f26e654714a7e13cf5056babd0c244c8cb
VIKAS SINGH <xanderbilla@gmail.com>
2023-04-26 04:18:11 +0530

    C1# Added List Component

---
+++ src/components/List.jsx
@@ -0,0 +1,21 @@
+import React, { useEffect} from 'react'
+import Product from './Product'
+import styles from '../styles/list.module.css'
+import { popularProducts } from '../static/data'
+
+const List = ({ cat, filters, sort }) => {
+    // const [products, setProducts] = useState([])
+    // const [filteredProducts, setFilteredProducts] = useState([])
+
+    useEffect(() => {
+
+    }, [cat])
+
+  return (
+      <div className={styles.list}>{popularProducts?.map(item => (
+          <Product item={item} key={ item.id}/>
+    ))}</div>
+  )
+}
+
+export default List
