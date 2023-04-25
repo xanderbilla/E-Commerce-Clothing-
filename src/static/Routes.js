f65ca898aad3528678365339a396cee52fcf1f3a
VIKAS SINGH <xanderbilla@gmail.com>
2023-04-26 04:22:20 +0530

    C1# Added Route

---
+++ src/static/Routes.js
@@ -0,0 +1,9 @@
+import Main from '../pages/Main';
+import ProductList from '../pages/ProductList';
+import ProductPage from '../pages/ProductPage';
+import ProfilePage from '../pages/ProfilePage';
+import LoginPage from '../pages/LoginPage';
+import SignUpPage from '../pages/SignUpPage';
+import Cart from '../pages/Cart';
+
+export {Cart, LoginPage, Main, SignUpPage, ProductList, ProductPage, ProfilePage}
