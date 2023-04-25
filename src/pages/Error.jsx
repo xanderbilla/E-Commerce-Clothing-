7cb8817c359d4c38128868db6484880a6adf0aed
VIKAS SINGH <xanderbilla@gmail.com>
2023-04-26 04:42:32 +0530

    C1# Added Error Page

---
+++ src/pages/Error.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import '../styles/error.css'
+const Error = () => {
+  return (
+    <main>
+  <h1>4<span>0</span>4</h1>
+  <h2>Error: 404 page not found</h2>
+  <p>Sorry, the page you're looking for cannot be accessed</p>
+</main>
+  )
+}
+
+export default Error
