4604b8f0e7463f42a007111fd24f29402c20d8f5
VIKAS SINGH <xanderbilla@gmail.com>
2023-04-26 04:17:29 +0530

    Dummy Static Data

---
+++ src/static/data.js
@@ -0,0 +1,178 @@
+export const sliderItems = [
+  {
+    id: 1,
+    img: "https://i.imgur.com/Jr1lNp9.png",
+    title: "SUMMER SALE",
+    desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+    bg: "f5fafd",
+  },
+  {
+    id: 2,
+    img: "https://i.imgur.com/yRBHphH.png",
+    title: "AUTUMN COLLECTION",
+    desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+    bg: "fcf1ed",
+  },
+  {
+    id: 3,
+    img: "https://i.imgur.com/G4t3S3c.png",
+    title: "LOUNGEWEAR LOVE",
+    desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+    bg: "fbf0f4",
+  },
+];
+
+export const categories = [
+  {
+    id: 1,
+    img: "https://images.pexels.com/photos/428338/pexels-photo-428338.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    title: "WOMEN",
+    cat: "Women"
+  },
+  {
+    id: 2,
+    img: "https://images.pexels.com/photos/1040945/pexels-photo-1040945.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    title: "MEN",
+    cat: "Men"
+  },
+  {
+    id: 3,
+    img: "https://images.pexels.com/photos/10508388/pexels-photo-10508388.jpeg?auto=compress&cs=tinysrgb&w=600",
+    title: "KIDS",
+    cat: "Kids"
+  },
+  {
+    id: 4,
+    img: "https://images.pexels.com/photos/1204459/pexels-photo-1204459.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    title: "ACESSORIES",
+    cat: "Accessories"
+  }
+];
+
+export const featuredProducts = [
+  {
+    _id: 1,
+    img: "https://images.pexels.com/photos/1972115/pexels-photo-1972115.jpeg?auto=compress&cs=tinysrgb&w=1600",
+    img2: "https://images.pexels.com/photos/1163194/pexels-photo-1163194.jpeg?auto=compress&cs=tinysrgb&w=1600",
+    title: "Long Sleeve Graphic T-Shirt",
+    isNew: true,
+    oldPrice: 19,
+    category: "Women",
+    price: 12,
+    size: ["M", "S", "XS"],
+    color: ["Yellow", "Black", "Green"]
+  },
+  {
+    _id: 2,
+    img: "https://images.pexels.com/photos/1759622/pexels-photo-1759622.jpeg?auto=compress&cs=tinysrgb&w=1600",
+    img2: "https://images.pexels.com/photos/1620765/pexels-photo-1620765.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    title: "Coat",
+    isNew: true,
+    oldPrice: 19,
+    category: "Women",
+    price: 12,
+    size: ["M", "L", "XL", "XS"],
+    color: ["Yellow", "Blue", "Red"]
+
+  },
+  {
+    _id: 3,
+    img: "https://images.pexels.com/photos/1457983/pexels-photo-1457983.jpeg?auto=compress&cs=tinysrgb&w=1600",
+    img2: null,
+    title: "Skirt",
+    oldPrice: 19,
+    category: "Women",
+    price: 12,
+    size: ["M", "L", "M"],
+    color: ["Red", "White", "Green"]
+  },
+  {
+    _id: 4,
+    img: "https://images.pexels.com/photos/2065200/pexels-photo-2065200.jpeg?auto=compress&cs=tinysrgb&w=1600",
+    title: "Trouser",
+    oldPrice: 19,
+    category: "Men",
+    price: 12,
+    size: ["M", "XL", "XS"],
+    color: ["Blue", "Black", "White"]
+  }
+]
+
+export const popularProducts = [
+  {
+    _id: 1,
+    img: "https://images.pexels.com/photos/1972115/pexels-photo-1972115.jpeg?auto=compress&cs=tinysrgb&w=1600",
+    img2: "https://images.pexels.com/photos/1163194/pexels-photo-1163194.jpeg?auto=compress&cs=tinysrgb&w=1600",
+    title: "Long Sleeve Graphic T-Shirt",
+    isNew: true,
+    oldPrice: 19,
+    category: "Women",
+    price: 12,
+    size: ["M", "S", "XS"],
+    color: ["Yellow", "Black", "Green"]
+  },
+  {
+    _id: 2,
+    img: "https://images.pexels.com/photos/1759622/pexels-photo-1759622.jpeg?auto=compress&cs=tinysrgb&w=1600",
+    img2: "https://images.pexels.com/photos/14637395/pexels-photo-14637395.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    title: "Coat",
+    isNew: true,
+    oldPrice: 19,
+    category: "Women",
+    price: 12,
+    size: ["M", "L", "XL", "XS"],
+    color: ["Yellow", "Blue", "Red"]
+
+  },
+  {
+    _id: 3,
+    img: "https://images.pexels.com/photos/14577038/pexels-photo-14577038.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    img2: "https://images.pexels.com/photos/1152994/pexels-photo-1152994.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    title: "Skirt",
+    oldPrice: 19,
+    category: "Women",
+    price: 12,
+    size: ["M", "L", "M"],
+    color: ["Red", "White", "Green"]
+  },
+  {
+    _id: 4,
+    img: "https://images.pexels.com/photos/2065200/pexels-photo-2065200.jpeg?auto=compress&cs=tinysrgb&w=1600",
+    title: "Trouser",
+    oldPrice: 19,
+    category: "Men",
+    price: 12,
+    size: ["M", "XL", "XS"],
+    color: ["Blue", "Black", "White"]
+  },
+  {
+    _id: 5,
+    img: "https://images.pexels.com/photos/769749/pexels-photo-769749.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    title: "Shirt",
+    oldPrice: 19,
+    category: "Men",
+    price: 12,
+    size: ["M", "S", "XS"],
+    color: ["Blue", "Yellow", "White"]
+  },
+  {
+    _id: 6,
+    img: "https://images.pexels.com/photos/1346187/pexels-photo-1346187.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    title: "Jeans",
+    oldPrice: 19,
+    category: "Men",
+    price: 12,
+    size: ["M", "S", "L", "XL"],
+    color: ["Black", "White"]
+  },
+  {
+    _id: 7,
+    img: "https://images.pexels.com/photos/984619/pexels-photo-984619.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    title: "Hat",
+    oldPrice: 23,
+    category: "Accessories",
+    price: 20,
+    size: ["M", "S"],
+    color: ["Blue"]
+  },
+]
