855e5a12a20653ba8c6071b9f19eb967c7fae627
VIKAS SINGH <xanderbilla@gmail.com>
2023-04-26 04:14:31 +0530

    App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { useState } from 'react';
-import { Cart, LoginPage, Main, SignUpPage, ProductList, ProductPage, User, Verify, ProfilePage } from './static/Routes';
+import { Cart, LoginPage, Main, SignUpPage, ProductList, ProductPage, Verify, ProfilePage } from './static/Routes';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import MiniNavbar from './components/MiniNavbar';
@@ -19,7 +19,6 @@ function App() {
             <Route path='cart' element={<Cart />} />
             <Route path='my-profile' element={<ProfilePage />} />
             <Route path='verify' element={<Verify />} />
-            <Route path='user' element={<User />} />
             <Route path='product/:productId' element={<ProductPage />} />
             <Route path='/category/:catName' >
               <Route index element={<ProductList />} />
