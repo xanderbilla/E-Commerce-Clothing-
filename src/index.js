d0e61deca2b44c33b20652e4ec73c3f4f8f902f1
VIKAS SINGH <xanderbilla@gmail.com>
2023-04-26 05:44:21 +0530

    Added AWS Export File

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Amplify} from 'aws-amplify';
 import App from './App';
+import awsconfig from './aws-exports';
+Amplify.configure(awsconfig);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
